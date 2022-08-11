04237a58714045366c8567f74a5a918320374c8f
Lucas Santos <15175437+Lucasxp32@users.noreply.github.com>
2022-08-11 19:13:35 -0300

    Make behavior like the original inspiration
    - Removes coloring
    - Add a new line after expression is invoked

--- replify.ps1
+++ replify.ps1
@@ -1,14 +1,15 @@
 function get_input($s){
-    Write-Host "$s>" -ForegroundColor white -BackgroundColor DarkGray -NoNewLine
+    Write-Host "$s>" -NoNewLine
     Write-Host " " -NoNewLine
     return $Host.UI.ReadLine()
 }
 
 $initial_prompt = $args[0]
-Write-Host "Initialized REPL for $initial_prompt" -ForegroundColor white -BackgroundColor DarkGray
+Write-Host "Initialized REPL for $initial_prompt"
 $prompt = get_input($initial_prompt)
 Do  
 {  
     Invoke-Expression "$initial_prompt $prompt"
+    Write-Host ""
     $prompt = get_input($initial_prompt)
 } until ($prompt -eq '')
